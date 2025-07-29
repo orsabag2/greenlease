4b7c9c849b8977c125688bd10b1ef06ab31ebe4c
Or Sabag <or.sabag@nilus.com>
2025-07-29 19:03:43 +0300

    Fix Firebase auth domain for development vs production

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,7 +4,9 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDetmgUsuXFd5S-b4-3CoUluh32WdRFvcI",
-  authDomain: "greenlease.me",
+  authDomain: typeof window !== 'undefined' && window.location.hostname === 'localhost' 
+    ? "lease-18ed6.firebaseapp.com" 
+    : "greenlease.me",
   projectId: "lease-18ed6",
   messagingSenderId: "740646580319",
   appId: "1:740646580319:web:ec51ad8ecd4e887471abea",
