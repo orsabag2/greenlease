7df648ecb23b0aedb5f467a8db016e2e6e47c7c5
Or Sabag <or.sabag@nilus.com>
2025-07-29 19:13:17 +0300

    Fix Firebase auth domain to use default Firebase domain

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,9 +4,7 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDetmgUsuXFd5S-b4-3CoUluh32WdRFvcI",
-  authDomain: typeof window !== 'undefined' && window.location.hostname === 'localhost' 
-    ? "lease-18ed6.firebaseapp.com" 
-    : "greenlease.me",
+  authDomain: "lease-18ed6.firebaseapp.com",
   projectId: "lease-18ed6",
   messagingSenderId: "740646580319",
   appId: "1:740646580319:web:ec51ad8ecd4e887471abea",
@@ -21,7 +19,12 @@ export const googleProvider = new GoogleAuthProvider();
 
 googleProvider.addScope('openid');
 googleProvider.addScope('email');
-// Remove custom redirect URI to let Firebase handle it automatically
-// googleProvider.setCustomParameters({
-//   redirect_uri: 'https://greenlease.me/api/auth/callback'
-// }); 
+
+// Set custom parameters for production domain
+if (typeof window !== 'undefined' && window.location.hostname === 'greenlease.me') {
+  googleProvider.setCustomParameters({
+    prompt: 'select_account',
+    // This will help show the custom domain in the modal
+    login_hint: 'greenlease.me'
+  });
+} 
